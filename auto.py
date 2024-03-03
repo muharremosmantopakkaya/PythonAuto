e92fd5c4f15270adc7743f4c1b490074ec205b70
muharremosmantopakkaya <95377072+muharremosmantopakkaya@users.noreply.github.com>
2024-03-03 13:58:54 +0300

    Açıklama

--- auto.py
+++ auto.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 
 def mesaj():
-    message = "MERHABA BEN MUHARREM"
+    message = "MERHABA BEN MUHARREM"   #Siz istediğiniz kelime veya cümleyi buraya yazabilirsiniz.
     pyautogui.typewrite(message)
     pyautogui.press("enter")
 
